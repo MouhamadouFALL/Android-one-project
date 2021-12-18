052dd16793e4f160d86261e26f5b4886a0b81a9a
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2021-12-18 19:29:48 +0000

    un projet plusieurs modules

---
+++ app/src/main/java/com/naby/put_one_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.naby.put_one_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
